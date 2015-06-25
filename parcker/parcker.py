237e33b920cabd050135bcc51e31d790cb6a11d3
mongolrgata <mongolrgata@gmail.com>
2015-06-25 07:28:39 +0300

    Черновая версия parcker.py

--- parcker/parcker.py
+++ parcker/parcker.py
@@ -1,13 +1,54 @@
 __author__ = 'mongolrgata'
 
 import os
+import sys
+import struct
 
-script_directory = os.path.dirname(os.path.abspath(__file__))
+
+def write_unsigned_int32(file, value):
+    return file.write(struct.pack('<L', value))
+
+
+def write_filename(file, filename):
+    for char in filename:
+        file.write(char.encode() + b'\x00')
+
+    return file.write(b'\x00\x00')
 
 
 def main():
-    # TODO
-    pass
+    file_names = []
+    file_sizes = []
+    content = b''
+
+    for filename in sys.argv[2:]:
+        file_names.append(os.path.basename(filename))
+        file_sizes.append(os.path.getsize(filename))
+
+        with open(filename, 'rb') as file_in:
+            content += file_in.read()
+
+    with open(sys.argv[1], 'wb+') as arc_file:
+        file_count = len(sys.argv) - 2
+
+        write_unsigned_int32(arc_file, file_count)
+        write_unsigned_int32(arc_file, 0)
+
+        file_offset = 0
+
+        for i in range(0, file_count):
+            write_unsigned_int32(arc_file, file_sizes[i])
+            write_unsigned_int32(arc_file, file_offset)
+            write_filename(arc_file, file_names[i])
+
+            file_offset += file_sizes[i]
+
+        header_length = arc_file.tell() - 2
+
+        arc_file.write(content)
+
+        arc_file.seek(4)
+        write_unsigned_int32(arc_file, header_length)
 
 
 if __name__ == '__main__':
