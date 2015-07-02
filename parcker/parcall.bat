6a3b3ac542a3cf1cb66e5f0f2949725de2fe58aa
mongolrgata <mongolrgata@gmail.com>
2015-07-03 00:30:24 +0300

    Без явных имен

--- parcker/parcall.bat
+++ parcker/parcall.bat
@@ -1,16 +1,2 @@
-python parcker.py BGM
-python parcker.py CHIP1
-python parcker.py CHIP2
-python parcker.py CHIP3
-python parcker.py CHIP4
-python parcker.py CHIP5
-python parcker.py CHIP6
-python parcker.py EFFECT
-python parcker.py GRAPHIC
-python parcker.py RIO
-python parcker.py SCRIPT
-python parcker.py SE
-python parcker.py SYSGRAPHIC
-python parcker.py SYSVOICE
-python parcker.py VOICE
+for %%A in (*.arc) do python parcker.py %%~nA
 pause
