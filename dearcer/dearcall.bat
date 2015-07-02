7ae39acb43a79d36c9884eee47c22dd6c9dd6d80
mongolrgata <mongolrgata@gmail.com>
2015-07-02 18:43:21 +0300

    *.bat

---
+++ dearcer/dearcall.bat
@@ -0,0 +1,16 @@
+python dearcer.py BGM.arc
+python dearcer.py CHIP1.arc
+python dearcer.py CHIP2.arc
+python dearcer.py CHIP3.arc
+python dearcer.py CHIP4.arc
+python dearcer.py CHIP5.arc
+python dearcer.py CHIP6.arc
+python dearcer.py EFFECT.arc
+python dearcer.py GRAPHIC.arc
+python dearcer.py RIO.arc
+python dearcer.py SCRIPT.arc
+python dearcer.py SE.arc
+python dearcer.py SYSGRAPHIC.arc
+python dearcer.py SYSVOICE.arc
+python dearcer.py VOICE.arc
+pause
---
+++ parcker/parcall.bat
@@ -0,0 +1,16 @@
+python parcker.py BGM
+python parcker.py CHIP1
+python parcker.py CHIP2
+python parcker.py CHIP3
+python parcker.py CHIP4
+python parcker.py CHIP5
+python parcker.py CHIP6
+python parcker.py EFFECT
+python parcker.py GRAPHIC
+python parcker.py RIO
+python parcker.py SCRIPT
+python parcker.py SE
+python parcker.py SYSGRAPHIC
+python parcker.py SYSVOICE
+python parcker.py VOICE
+pause
