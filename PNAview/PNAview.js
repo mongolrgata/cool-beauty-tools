734f46af20cc2209e444d189534c85ff1caea73d
mongolrgata <mongolrgata@gmail.com>
2015-07-07 00:50:33 +0300

    PNAview

--- PNAview/PNAview.js
+++ PNAview/PNAview.js
@@ -4,15 +4,17 @@
 
 $(document).ready(function () {
     var $layerInfoTemplate = $([
-        '<tr class="layer-info">                             ',
-        '   <td><input type="checkbox"></td>                 ',
-        '   <td><div class="preview"></div></td>             ',
-        '   <td align="right"><span class="layer-width"></td>',
-        '   <td>&#x2715;</td>                                ',
-        '   <td align="left"><span class="layer-height"></td>',
-        '   <td><span class="offset-left"></td>              ',
-        '   <td><span class="offset-top"></td>               ',
-        '</tr>                                               '
+        '<tr class="layer-info">                                  ',
+        '   <td><input class="visible-check" type="checkbox"></td>',
+        '   <td><div class="preview"></div></td>                  ',
+        '   <td align="right"><span class="layer-width"></td>     ',
+        '   <td>&#x2715;</td>                                     ',
+        '   <td align="left"><span class="layer-height"></td>     ',
+        '   <td>&#x394;X:</td>                                    ',
+        '   <td align="right"><span class="offset-left"></td>     ',
+        '   <td>&#x394;Y:</td>                                    ',
+        '   <td align="right"><span class="offset-top"></td>      ',
+        '</tr>                                                    '
     ].join('\n'));
 
     $('#PNA-file').change(function () {
@@ -25,24 +27,54 @@ $(document).ready(function () {
             var binaryFile = new BinaryFile(binaryString);
             var pnaObject = new PNA(binaryFile);
 
+            var $images = $('#images').empty();
+            var $background = $('<div/>').css({
+                position: 'relative',
+                width: pnaObject.imageWidth,
+                height: pnaObject.imageHeight,
+                background: '' +
+                'linear-gradient(135deg, transparent 75%, lightgray 0%) 0 0,    ' +
+                'linear-gradient(-45deg, transparent 75%, lightgray 0%) 8px 8px,' +
+                'linear-gradient(135deg, transparent 75%, lightgray 0%) 8px 8px,' +
+                'linear-gradient(-45deg, transparent 75%, lightgray 0%) 0 0     ',
+                backgroundSize: '16px 16px'
+            });
+
             var $layers = $('#layers').empty();
-            var $table = $('<table style="border-collapse: collapse;"/>');
+            var $table = $('<table/>').css({
+                borderCollapse: 'collapse'
+            });
 
             for (var i = 0, n = pnaObject.layers.length; i < n; ++i) {
-                var $layerInfo = $layerInfoTemplate.clone();
                 var layer = pnaObject.layers[i];
 
+                var $img = $('<img>').prop({
+                    src: layer.pngDataUrl
+                }).css({
+                    position: 'absolute',
+                    left: layer.offsetLeft,
+                    top: layer.offsetTop,
+                    zIndex: layer.zIndex
+                }).hide();
+
+                var $layerInfo = $layerInfoTemplate.clone();
+
+                $layerInfo.find('.visible-check').change({linkedImage: $img}, function (event) {
+                    event.data.linkedImage.toggle($(this).prop('checked'));
+                });
                 $layerInfo.find('.preview').css({
                     backgroundImage: ['url(', layer.pngDataUrl, ')'].join('')
                 });
                 $layerInfo.find('.layer-width').text(layer.layerWidth);
                 $layerInfo.find('.layer-height').text(layer.layerHeight);
-                $layerInfo.find('.offset-left').text('\u0394X:' + layer.offsetLeft);
-                $layerInfo.find('.offset-top').text('\u0394Y:' + layer.offsetTop);
+                $layerInfo.find('.offset-left').text(layer.offsetLeft);
+                $layerInfo.find('.offset-top').text(layer.offsetTop);
 
+                $background.append($img);
                 $table.append($layerInfo);
             }
 
+            $images.append($background);
             $layers.append($table);
         };
 
