6a87e08e09fd0b6c56387126ba66351e95f08f3b
mongolrgata <mongolrgata@gmail.com>
2015-07-16 08:35:31 +0300

    Поправлено перекрытие имён

--- byteshift/byteshift.py
+++ byteshift/byteshift.py
@@ -3,17 +3,17 @@ __author__ = 'mongolrgata'
 import sys
 
 
-def rotr8(int8, shift):
+def rotr8(int8, shift_size):
     """
     :param int8:
     :type int8: int
-    :param shift:
-    :type shift: int
+    :param shift_size:
+    :type shift_size: int
     :return:
     :rtype: int
     """
 
-    return (int8 >> shift) | (int8 << (8 - shift) & 0xff)
+    return (int8 >> shift_size) | (int8 << (8 - shift_size) & 0xff)
 
 
 def shift_decode(string):
@@ -32,17 +32,17 @@ def shift_decode(string):
     return bytes(result)
 
 
-def rotl8(int8, shift):
+def rotl8(int8, shift_size):
     """
     :param int8:
     :type int8: int
-    :param shift:
-    :type shift: int
+    :param shift_size:
+    :type shift_size: int
     :return:
     :rtype: int
     """
 
-    return (int8 << shift) & 0xff | (int8 >> (8 - shift))
+    return (int8 << shift_size) & 0xff | (int8 >> (8 - shift_size))
 
 
 def shift_encode(string):
