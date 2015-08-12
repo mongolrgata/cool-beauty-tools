8894bcc447e005c0e6dab107910efc67eaa0340b
mongolrgata <mongolrgata@gmail.com>
2015-08-12 21:05:47 +0300

    prepare.bat

---
+++ prepare.bat
@@ -0,0 +1,8 @@
+mklink ".\.ws2-tools\w2jImport\byteshift.py" "..\byteshift\byteshift.py"
+mklink ".\.ws2-tools\w2jImport\dearcer.py" "..\..\.arc-tools\dearcer\dearcer.py"
+mklink ".\.ws2-tools\w2jImport\parcker.py" "..\..\.arc-tools\parcker\parcker.py"
+
+mklink ".\localeFixer\dearcer.py" "..\.arc-tools\dearcer\dearcer.py"
+mklink ".\localeFixer\parcker.py" "..\.arc-tools\parcker\parcker.py"
+
+pause
