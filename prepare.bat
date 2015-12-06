921352d7b06e65faed9958798b7422ba5bd16908
mongolrgata <mongolrgata@gmail.com>
2015-12-06 14:43:11 +0300

    Документация

--- prepare.bat
+++ prepare.bat
@@ -2,7 +2,4 @@ mklink ".\.ws2-tools\w2jImport\byteshift.py" "..\byteshift\byteshift.py"
 mklink ".\.ws2-tools\w2jImport\dearcer.py" "..\..\.arc-tools\dearcer\dearcer.py"
 mklink ".\.ws2-tools\w2jImport\parcker.py" "..\..\.arc-tools\parcker\parcker.py"
 
-mklink ".\localeFixer\dearcer.py" "..\.arc-tools\dearcer\dearcer.py"
-mklink ".\localeFixer\parcker.py" "..\.arc-tools\parcker\parcker.py"
-
 pause
